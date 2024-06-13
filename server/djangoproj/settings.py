94f3001e1ec9170a7b0e23e69c9710c9c282f1c8
wangxi <wangxi009567@sina.com>
2024-06-13 12:15:15 +0800

    initialize the about page

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = \
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1',
                  'https://wangxijordan-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
 CSRF_TRUSTED_ORIGINS = ['https://wangxijordan-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
 
