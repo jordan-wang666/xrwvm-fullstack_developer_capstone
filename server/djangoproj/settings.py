1fe57d53ad2c16d37dc66ae63411109b4658343a
wangxi <wangxi009567@sina.com>
2024-06-13 16:08:02 +0800

    modify setting url

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,10 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "https://wangxijordan-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "https://wangxijordan-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://wangxijordan-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"
+    "https://wangxijordan-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
 ]
 
 REST_FRAMEWORK = {
