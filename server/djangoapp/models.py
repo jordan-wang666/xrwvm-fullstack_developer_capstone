476a46830f20645ad8b931d256887577ee3eb9c4
wangxi <wangxi009567@sina.com>
2024-06-13 19:34:45 +0800

    modify github lint

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
+
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -16,6 +16,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
     # Other fields as needed
 
     def __str__(self):
@@ -44,8 +45,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
     year = models.IntegerField(
-        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+        default=2023,
+        validators=[MaxValueValidator(2023),
+                    MinValueValidator(2015)]
     )
+
     # Other fields as needed
 
     def __str__(self):
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,35 +2,35 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 
 from . import views
 
 app_name = "djangoapp"
 urlpatterns = [
-    # # path for registration
-    # path for login
-    path(route="login", view=views.login_user, name="login"),
-    path(route="logout", view=views.logout_request, name="logout"),
-    path(route="register", view=views.registration, name="register"),
-    path(route="get_cars", view=views.get_cars, name="getcars"),
-    # path for dealer reviews view
-    path(route="get_dealers", view=views.get_dealerships, name="get_dealers"),
-    path(
-        route="get_dealers/<str:state>",
-        view=views.get_dealerships,
-        name="get_dealers_by_state",
-    ),
-    path(
-        route="dealer/<str:dealer_id>",
-        view=views.get_dealer_details,
-        name="dealer",
-    ),
-    path(
-        route="reviews/dealer/<str:dealer_id>",
-        view=views.get_dealer_reviews,
-        name="get_dealer_reviews_by_id",
-    ),
-    # path for add a review view
-    path(route="add_review", view=views.add_review, name="add_review"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  # # path for registration
+                  # path for login
+                  path(route="login", view=views.login_user, name="login"),
+                  path(route="logout", view=views.logout_request, name="logout"),
+                  path(route="register", view=views.registration, name="register"),
+                  path(route="get_cars", view=views.get_cars, name="getcars"),
+                  # path for dealer reviews view
+                  path(route="get_dealers", view=views.get_dealerships, name="get_dealers"),
+                  path(
+                      route="get_dealers/<str:state>",
+                      view=views.get_dealerships,
+                      name="get_dealers_by_state",
+                  ),
+                  path(
+                      route="dealer/<str:dealer_id>",
+                      view=views.get_dealer_details,
+                      name="dealer",
+                  ),
+                  path(
+                      route="reviews/dealer/<str:dealer_id>",
+                      view=views.get_dealer_reviews,
+                      name="get_dealer_reviews_by_id",
+                  ),
+                  # path for add a review view
+                  path(route="add_review", view=views.add_review, name="add_review"),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
